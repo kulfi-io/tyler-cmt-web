5ebc9cae62ea06bdcd436d266c44b8797a373d33
ashish chatterjee <ashish@kulfi.io>
2019-06-25 17:35:54 -0700

    removed fullpage and corresponding data components from privacy view

--- src/views/privacy.vue
+++ src/views/privacy.vue
@@ -9,35 +9,15 @@
 <script lang="ts">
 import Vue from "vue";
 import PrivacyVue from "../components/privacy-vue.vue";
-import FullPage from "vue-fullpage.js/src/FullPage.vue";
 import HeaderVue from "../components/header-vue.vue";
 import FooterVue from '../components/footer-vue.vue';
-import Shared from '../shared';
 
 export default Vue.extend({
   name: "privacy",
   components: {
     HeaderVue,
-    FullPage,
     PrivacyVue,
     FooterVue
-  },
-  data() {
-    return {
-      options: {
-        scrollOverflow: false,
-        scrollBar: true,
-        menu: "#menu",
-        navigation: true,
-        anchors: ["privacy"],
-        sectionsColor: ["#fff"],
-        licenseKey: "002D6FD2-0EC343D4-9403ACA4-CB7CDBFB"
-      }
-    };
-  },
-  mounted: function() {
-    const _target = <HTMLDivElement>document.querySelector('.swap-position');
-    Shared.methods.setCellPositionTop(_target);
   }
 });
 </script>
