7c340e1ebad02bd21a1d4180311635c3eca1a3c1
ashish chatterjee <ashish@kulfi.io>
2019-06-25 17:51:06 -0700

    added about related scafolding

---
+++ src/views/about.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+    div.about
+        HeaderVue
+        AboutVue
+    //- FooterVue
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import AboutVue from '../components/about-vue.vue';
+import HeaderVue from '../components/header-vue.vue';
+import FooterVue from '../components/footer-vue.vue';
+export default Vue.extend({
+    name: 'about',
+    components: {
+        HeaderVue,
+        AboutVue,
+        FooterVue
+    },
+    
+})
+</script>
+
