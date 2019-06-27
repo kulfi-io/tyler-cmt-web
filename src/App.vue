b2a536b15cd17cc0f3d5ffd2112e0cc6872a80fd
ashish chatterjee <ashish@kulfi.io>
2019-06-27 16:27:11 -0700

    removed page transition

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <template>
   <div id="app">
-    <transition name="fade" mode="out-in">
-      <router-view></router-view>
-    </transition>
+    <router-view></router-view>
   </div>
 </template>
 
@@ -11,16 +9,5 @@ import Vue from "vue";
 import Home from "./views/home.vue";
 export default Vue.extend({
   name: "App",
-  
 });
 </script>
-
-<style scoped>
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.5s;
-}
-.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
-  opacity: 0;
-}
-</style>
