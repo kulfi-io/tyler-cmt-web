92ba8554b5f8a8744c8410145aede0ccef48015b
ashish chatterjee <ashish@kulfi.io>
2019-06-27 17:01:46 -0700

    moved vue styles to scss file and changed about blockquote default font size

--- src/components/intro-about.vue
+++ src/components/intro-about.vue
@@ -21,35 +21,3 @@ export default Vue.extend({
   }
 });
 </script>
-
-<style lang="scss" scoped>
-.about-animation-vue {
-  
-  color: #515769;
-  padding-top: 10%;
-  padding-left: 12%;
-  padding-right: 10%;
-  padding-bottom: 5%;
-  animation-duration: 1s;
-  animation-name: fadeIn;
-  animation-timing-function: cubic-bezier(0.71, 0.55, 0.62, 1.57);
-
-  blockquote {
-    margin: 0.5rem 0 1rem;
-    position: relative;
-    font-family: "Sanchez", serif;
-    font-size: 2em;
-    line-height: 1.5em;
-    font-style: italic;
-  }
-
-  .detail {
-    text-decoration: none;
-    font-size: small;
-    font-family: sans-serif;
-    text-transform: capitalize;
-    white-space: nowrap;
-  }
-}
-</style>
-
