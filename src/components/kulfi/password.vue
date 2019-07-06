9393c0d825393d8cd3e0b4f11fc0ed5a96b0139e
ashish chatterjee <ashish@kulfi.io>
2019-07-05 17:43:56 -0700

    added login and supporting components

---
+++ src/components/kulfi/password.vue
@@ -0,0 +1,88 @@
+<template lang="pug">
+  div.kulfi-password
+    label.control-label( :for="tag") {{ label }}
+      span.required-field *
+    div.input-group
+      div.input-group-prepend
+        span.input-group-text( class="input-icon-btn" :id="`toggle-${tag}`"
+        :data-target="tag" data-display="hidden")
+          font-awesome-icon.fa( id="fa-pwd-eye" class="fa-eye fa-base" :icon="iconEye" aria-hidden="true")
+      input.form-control(type="password" :id="tag" 
+      :ref="tag" :title="title" :placeholder="placeholder" 
+      required pattern="[a-zA-Z0-9!@#$%]{8,15}")
+      div.input-group-prepend
+        span.input-group-text(class="left")
+          font-awesome-icon.fa( class="fa-pwd fa-base" :icon="iconLock" aria-hidden="true")
+</template>
+
+
+<script lang="ts">
+import Vue from "vue";
+import Library from "../../library/account";
+import { faLock, faEye } from "@fortawesome/fontawesome-free-solid";
+
+export default Vue.extend({
+  name: "kulfi-password",
+  props: ["tag", "title", "placeholder", "readyToSubmit", "label"],
+  computed: {
+    iconLock: function() {
+      return faLock;
+    },
+    iconEye: function() {
+      return faEye;
+    }
+  },
+  methods: {
+    allowValidPwdCharacters: function(event: KeyboardEvent): boolean {
+      this.$parent.$data.matched = false;
+      let _return = true;
+
+      if (
+        !(event.which >= 64 && event.which <= 90) &&
+        !(event.which >= 97 && event.which <= 122) &&
+        !(event.which == 33) &&
+        !(event.which >= 35 && event.which <= 37) &&
+        !(event.which >= 48 && event.which <= 57)
+      ) {
+        _return = false;
+      }
+
+      return _return;
+    },
+    setMatched: function(matched: boolean, target: HTMLElement): void {
+      const _matched = this.$parent.$data.matched;
+      matched ? Library.passed(target) : Library.muted(target);
+
+      if (_matched == null || _matched) {
+        this.$parent.$data.matched = matched;
+      }
+    },
+    validatePassword: function(elm: HTMLInputElement): boolean {
+      var _length = <HTMLElement>document.getElementById("pwd-length");
+      var _upper = <HTMLElement>document.getElementById("pwd-upper");
+      var _lower = <HTMLElement>document.getElementById("pwd-lower");
+      var _number = <HTMLElement>document.getElementById("pwd-number");
+      var _special = <HTMLElement>document.getElementById("pwd-special");
+
+      var _passFa = document.getElementById("fa-pwd");
+
+      var _value = elm.value;
+      if (_value.length > 15) return false;
+
+      this.setMatched(_value.length >= 8, _length);
+      this.setMatched(Library.validateLowerAlpha(_value), _lower);
+      this.setMatched(Library.validateUpperAlpha(_value), _upper);
+      this.setMatched(Library.validateDigit(_value), _number);
+      this.setMatched(Library.validateSpecial(_value), _special);
+
+      this.readyToSubmit();
+
+      this.$parent.$data.matched
+        ? Library.passed(_passFa)
+        : Library.muted(_passFa);
+
+      return this.$parent.$data.matched;
+    }
+  }
+});
+</script>
---
+++ src/components/kulfi/username.vue
@@ -0,0 +1,78 @@
+<template lang="pug">
+  div.kulfi-username
+    label.control-label( for="username") username
+      span.required-field *
+    div.input-group( role="group")
+      input.form-control(type="text" id="username" ref="username" 
+      title="username is required" placeholder="username" 
+      required 
+      pattern="[a-zA-Z0-9-_]{5,15}\w+")
+      div.input-group-prepend
+        span.input-group-text( class="left")
+          font-awesome-icon.fa( class="fa-base" :icon="iconUser")
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Library from '../../library/account';
+import {faUser} from '@fortawesome/fontawesome-free-solid';
+
+export default Vue.extend({
+  name: "kulfi-username",
+  props: ["readyToSubmit"],
+  computed: {
+    iconUser: function(){
+      return faUser;
+    }
+  },
+  mounted: function() {
+    var _self = this;
+    var _username = this.$el.querySelector("#username");
+
+    if (_username) {
+      _username.addEventListener("keyup", function(e: Event) {
+        _self.validateUsername(e.currentTarget as HTMLInputElement);
+      });
+
+      _username.addEventListener("keypress", function(e: Event) {
+        var _return = _self.validateInput(e as KeyboardEvent);
+        if (!_return) {
+          e.preventDefault();
+        }
+      });
+    }
+  },
+  methods: {
+    validateInput: function(e: KeyboardEvent): boolean {
+      this.$parent.$data.matched = false;
+      let _return = true;
+
+      if (
+        !(e.which >= 64 && e.which <= 90) &&
+        !(e.which >= 97 && e.which <= 122) &&
+        !(e.which >= 48 && e.which <= 57) &&
+        !(e.which == 45) &&
+        !(e.which == 95)
+      ) {
+        _return = false;
+      }
+
+      return _return;
+    },
+    validateUsername: function(elm: HTMLInputElement): boolean {
+      var _userFa = document.getElementById("fa-user");
+      var _value = elm.value;
+      if (_value.length > 15) return false;
+
+      this.$parent.$data.matched = _value.length >= 5;
+      this.$parent.$data.matched
+        ? Library.passed(_userFa)
+        : Library.muted(_userFa);
+
+      this.readyToSubmit();
+      return this.$parent.$data.matched;
+    }
+  }
+});
+</script>
+
