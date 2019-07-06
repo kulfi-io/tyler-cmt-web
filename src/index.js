8e550af806e13f156228fed3a36610080006003b
ashish chatterjee <ashish@kulfi.io>
2019-07-05 17:36:33 -0700

    removed bootstrap and added fontawesome

--- src/index.js
+++ src/index.js
@@ -6,19 +6,15 @@ import './assets/sass/base.scss';
 import './assets/sass/media.scss';
 import 'fullpage.js/dist/fullpage.css';
 import 'fullpage.js/vendors/scrolloverflow';
-import bootstrap from 'bootstrap-vue';
 import VueFullPage from 'vue-fullpage.js/dist/vue-fullpage';
 import animate from 'animate.css';
-
-import 'bootstrap-vue/dist/bootstrap-vue.min.css';
-
-
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome';
+Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 
 Vue.config.productionTip = false;
 Vue.use(VueFullPage);
 Vue.use(animate);
-Vue.use(bootstrap);
 
 new Vue({
     el: '#app',
