9e4f90c41be937f89174fd32c9a683047bd83942
ashish chatterjee <ashish@kulfi.io>
2019-07-05 17:42:47 -0700

    added login components

--- src/routes.ts
+++ src/routes.ts
@@ -3,13 +3,15 @@ import Router from 'vue-router';
 import Home from './views/home.vue';
 import Privacy from './views/privacy.vue';
 import About from './views/about.vue';
+import Login from './views/login.vue';
 
 Vue.use(Router);
 
 let routes = [
     { path: '/', name: 'home', title: 'home', component: Home},
     { path: '/privacy', name: 'privacy', title: 'privacy', component: Privacy},
-    { path: '/about', name: 'about', title: 'about', component: About}
+    { path: '/about', name: 'about', title: 'about', component: About},
+    { path: '/login', name: 'login', title: 'login', component: Login}
 
 ]
 
---
+++ src/views/login.vue
@@ -0,0 +1,40 @@
+<template lang="pug">
+    div.account-vue
+        HeaderVue
+        div.login-vue
+            FullPage.fullpage( :options="options")
+                div(class="section page fp-login ")
+                    AccountLogin
+        FooterVue
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import AccountLogin from '../components/account-login.vue';
+import HeaderVue from '../components/header-vue.vue';
+import FooterVue from '../components/footer-vue.vue';
+import FullPage from 'vue-fullpage.js/src/FullPage.vue';
+export default Vue.extend({
+    name: 'login',
+    components: {
+        AccountLogin,
+        HeaderVue,
+        FooterVue,
+        FullPage
+    },
+    data() {
+    return {
+      options: {
+        scrollOverflow: false,
+        scrollBar: false,
+        menu: "#menu",
+        navigation: false,
+        anchors: ["fp-login"],
+        sectionsColor: ["#C8C8C8"],
+        licenseKey: "002D6FD2-0EC343D4-9403ACA4-CB7CDBFB"
+      }
+    };
+  },
+})
+</script>
+
