21978fde0b98781bed411b8b42996405d1e7be7a
ashish chatterjee <ashish@kulfi.io>
2019-07-16 17:40:28 -0700

    removed ts-dist directory

--- ts-dist/library/eventinput.js
+++
@@ -1,9 +0,0 @@
-export default class EventInput {
-    constructor(title, description, start, end) {
-        this.title = title;
-        this.description = description;
-        this.start = start.replace('\"', '').replace('\"', '');
-        this.end = end.replace('\"', '').replace('\"', '');
-    }
-}
-//# sourceMappingURL=eventinput.js.map
--- ts-dist/library/user.js
+++
@@ -1,9 +0,0 @@
-export default class User {
-    constructor(id, firstName, lastName) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.fullName = `${this.firstName} ${this.lastName}`;
-    }
-}
-//# sourceMappingURL=user.js.map
